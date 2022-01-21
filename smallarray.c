3726f283e078a219a7905ae662b5e2625a083953
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-01-21 19:11:08 +0530

    Add files via upload

---
+++ smallarray.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int i,n,small=0;
+	scanf("%d",&n);
+	int num[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&num[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	
+	if(small>num[i])
+	{
+		small=num[i];
+	}
+	else
+	{
+		small=small;
+	}
+}
+printf("%d",small);
+}
